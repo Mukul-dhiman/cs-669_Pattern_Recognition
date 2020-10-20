1221314959d8980260e8b9063de8ca90c1f91820
Mukul-dhiman <orton1615@gmail.com>
2020-10-20 18:55:18 +0530

    desplating data A.py assighnment-1

--- assighnment1/question_A/A.py
+++ assighnment1/question_A/A.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+import matplotlib.pyplot as pt
+
 #read class1 data
 data_class1 = open("../linearly_seperable_data/Class1.txt",'r')
 
@@ -33,3 +36,46 @@ while line:
     line=data_class2.readline()
     
 data_class2.close()
+
+
+data_class1_list = np.array(data_class1_list)
+data_class2_list = np.array(data_class2_list)
+
+
+
+print("----------------Initial Data----------------------")
+pt.scatter(data_class1_list[:,0],data_class1_list[:,1],c='red',label='class1')
+pt.scatter(data_class2_list[:,0],data_class2_list[:,1],c='green',label='class2')
+pt.show()
+#print(data_class1_list)
+
+
+
+np.random.shuffle(data_class1_list)
+np.random.shuffle(data_class2_list)
+
+data_class1_list_70_per = data_class1_list[:700]
+data_class2_list_70_per = data_class2_list[:700]
+
+
+
+data_class1_list_30_per = data_class1_list[700:]
+data_class2_list_30_per = data_class2_list[700:]
+
+
+
+
+
+print("--------------70% of Initial Data-------------------")
+pt.scatter(data_class1_list_70_per[:,0],data_class1_list_70_per[:,1],c='red',label='class1')
+pt.scatter(data_class2_list_70_per[:,0],data_class2_list_70_per[:,1],c='green',label='class2')
+pt.show()
+
+
+
+
+
+
+
+
+
