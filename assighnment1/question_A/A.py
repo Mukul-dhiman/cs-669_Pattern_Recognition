700cbbdda5b7a661bae09224d13e00ac7d937562
Mukul-dhiman <orton1615@gmail.com>
2020-10-20 18:32:32 +0530

    reading data A.py assighnment-1

--- assighnment1/question_A/A.py
+++ assighnment1/question_A/A.py
@@ -1,2 +1,35 @@
-# -*- coding: utf-8 -*-
 
+#read class1 data
+data_class1 = open("../linearly_seperable_data/Class1.txt",'r')
+
+#read class2 data
+data_class2 = open("../linearly_seperable_data/Class2.txt",'r')
+
+
+# List of class1 and class2 data 
+data_class1_list = []
+data_class2_list = []
+
+
+
+
+line = data_class1.readline()
+while line:
+    line = line.strip().split("\t")
+    line[0] = float(line[0])
+    line[1] = float(line[1])
+    data_class1_list.append(line)
+    line=data_class1.readline()
+    
+data_class1.close()
+
+line = data_class2.readline()
+
+while line:
+    line = line.strip().split("\t")
+    line[0] = float(line[0])
+    line[1] = float(line[1])
+    data_class2_list.append(line)
+    line=data_class2.readline()
+    
+data_class2.close()
